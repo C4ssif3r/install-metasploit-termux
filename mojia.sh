7838bfa1da8f5aba21d0e1bc40f5714df3784cad
⥏ϻJi <79422726+C4ssif3r@users.noreply.github.com>
2024-03-04 19:03:38 +0330

    Update mojia.sh

--- mojia.sh
+++ mojia.sh
@@ -1,22 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Banner and center functions
-center_banner() {
-    local termwidth=$(stty size | cut -d" " -f2)
+mojia_banner() {
 
     local banner=(
-    "           ┳┳┓  ••        
-                ┃┃┃┏┓┓┓┏┓      
-                ┛ ┗┗┛┃┗┗┻      
-┳┳┓        ┓  •   ┳  ┛   ┓┓    
-┃┃┃┏┓╋┏┓┏┏┓┃┏┓┓╋  ┃┏┓┏╋┏┓┃┃┏┓┏┓
-┛ ┗┗ ┗┗┻┛┣┛┗┗┛┗┗  ┻┛┗┛┗┗┻┗┗┗ ┛ 
-         ┛                     "
-    )
+"┳┳┓  ••"
+"┃┃┃┏┓┓┓┏┓"
+"┛ ┗┗┛┃┗┗┻ "
+"┳┳┓        ┓  •   ┳  ┛   ┓┓"
+"┃┃┃┏┓╋┏┓┏┏┓┃┏┓┓╋  ┃┏┓┏╋┏┓┃┃┏┓┏┓"
+"┛ ┗┗ ┗┗┻┛┣┛┗┗┛┗┗  ┻┛┗┛┗┗┻┗┗┗ ┛ "
+"         ┛"
+)
 
     echo -e "\e[34m" # Blue color
     for line in "${banner[@]}"; do
-        printf "%*s\n" $(( (termwidth + ${#line}) / 2 )) "$line"
+        printf "%*s\n" $(( (${#line}) / 2 )) "$line"
     done
     echo -e "\e[0m" # Reset color
 }
@@ -28,41 +27,43 @@ center() {
 }
 
 clear
-center_banner
+mojia_banner
 
-# Loading spinner
-center "Loading..."
+
+center "PLZ WAIT ...\nMojia going to install msf6..."
 source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
 
-# Dependencies Installation
-center "* Dependencies installation..."
+# Dependencies
+center "★ mojia now go to installing Dependencies..."
 pkg update -y
 pkg upgrade -y -o Dpkg::Options::="--force-confnew"
 pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
 python3 -m pip install requests
 
-# Fix ruby BigDecimal 
+# Fix .rb Big Decimal 
 #center "* Fix ruby BigDecimal"
 #source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
 
-# Erase Old Metasploit Folder
+# del old msf
 center "* Erasing old metasploit folder..."
 if [ -d "${PREFIX}/opt/metasploit-framework" ]; then
   rm -rf ${PREFIX}/opt/metasploit-framework
 fi
 
-# Download Metasploit
-center "* Downloading..."
+# Download Msf 6
+center "★ mojia now Downloading msf6 ..."
 if [ ! -d "${PREFIX}/opt" ]; then
   mkdir ${PREFIX}/opt
 fi
 git clone https://github.com/rapid7/metasploit-framework.git --depth=1 ${PREFIX}/opt/metasploit-framework
 
-# Install Metasploit
-center "* Installation..."
+# Install
+center "* Mojia msf installer ..."
 cd ${PREFIX}/opt/metasploit-framework
 gem install bundler
 NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+
+
 # by overriding cflags nokogiri will install or you can simply declare a void function 
 #  you might have seen this error while installing nokogiri `xmlSetStructuredErrorFunc((void *)rb_error_list, Nokogiri_error_array_pusher);`
 #  solution : void xmlSetStructuredErrorFunc(void *rb_error_list, void *Nokogiri_error_array_pusher); you can set any parameter name 
@@ -75,13 +76,14 @@ bundle update activesupport
 bundle update --bundler
 bundle install -j$(nproc --all)
 
-# Link Metasploit Executables
+# Msf linking
 ln -sf ${PREFIX}/opt/metasploit-framework/msfconsole ${PREFIX}/bin/
 ln -sf ${PREFIX}/opt/metasploit-framework/msfvenom ${PREFIX}/bin/
 ln -sf ${PREFIX}/opt/metasploit-framework/msfrpcd ${PREFIX}/bin/
 termux-elf-cleaner ${PREFIX}/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
 
-echo -e "\033[32m" # Blue color
-center "Installation complete"
-echo -e "\nStart Metasploit using the command: msfconsole"
-echo -e "\033[0m" # Reset color
+# print success 
+echo -e "\033[32m" 
+center "Mojia msf Installer complete✓"
+echo -e "\nRUN Metasploit using this command: msfconsole"
+echo -e "\033[0m"
